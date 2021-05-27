6809c4f7862d49fd2bf9cd3bcc7179d9495fa457
Red and Green <62441426+RGGH@users.noreply.github.com>
2021-05-27 14:47:15 +0100

    Create items.py

---
+++ jobsites/items.py
@@ -0,0 +1,13 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class JobsItem(scrapy.Item):
+
+    url = scrapy.Field()
+    desc = scrapy.Field()
+
